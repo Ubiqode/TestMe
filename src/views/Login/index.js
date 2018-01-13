9e2e752eb1a9d03dfea232c3f6e4ef989dadc797
Henry Paulino <henrypl360@gmail.com>
2018-01-13 15:00:40 -0500

    Add eslint

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import styles from './styles'
 
-class Login extends Component {
-  render() {
-    return (
-			<View style={styles.container}>
-				<Text style={styles.testMe}>TestMe</Text>
-				<View style={styles.loginBox}>
-					<TextInput
-						placeholder="Email"
-					/>
-					<TextInput
-						placeholder="Password"
-					/>
+const Login = () => (
+	<View style={styles.container}>
+		<Text style={styles.testMe}>TestMe</Text>
+		<View style={styles.loginBox}>
+			<TextInput
+				placeholder="Email"
+			/>
+			<TextInput
+				placeholder="Password"
+			/>
 
-					<View style={styles.loginBtnContainer}>
-						<TouchableOpacity style={styles.loginBtn}>
-							<Text>LOGIN</Text>
-						</TouchableOpacity>
-					</View>
-        </View>
-      </View>
-    )
-  }
-}
+			<View style={styles.loginBtnContainer}>
+				<TouchableOpacity style={styles.loginBtn}>
+					<Text>LOGIN</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	</View>
+)
 
 export default Login
