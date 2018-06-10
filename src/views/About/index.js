5e41b8ea2b35f44c8fbe4e2bc75cd6f387d5c815
Henry Paulino <henrypl360@gmail.com>
2018-06-10 18:32:52 -0400

    fix indentation

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash'
 import {
-  Image,
-  Platform,
-  Text,
-  TouchableOpacity,
-  View,
+	Image,
+	Platform,
+	Text,
+	TouchableOpacity,
+	View,
 } from 'react-native'
 import { observer } from 'mobx-react'
 import { AboutStore } from '@stores'
@@ -17,51 +17,51 @@ import styles from './styles'
 
 @observer
 export default class About extends Component {
-  constructor(props) {
-    super(props)
-    this.store = new AboutStore()
-  }
+	constructor(props) {
+		super(props)
+		this.store = new AboutStore()
+	}
 
-  render() {
-    const navHeightPadding = { marginTop: Platform.OS === 'ios' ? 64 : 54 }
-    const developer = TEAM[this.state.currentTab]
-    return (
-      <View style={[styles.container, navHeightPadding]}>
-        <Image style={styles.profileImage} source={{ uri: developer.avatar }} />
-        <Image source={{ uri: developer.header }} style={styles.headerImage} />
-        <View style={styles.content}>
-          <Text style={styles.contentName}>{developer.name}</Text>
-          <Text style={styles.contentTitle}>{developer.title}</Text>
-          <View style={styles.socialButtons}>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => this.store.gotoLink(developer.github)}
-            >
-              <GithubButton />
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => this.store.gotoLink(developer.website)}
-            >
-              <WebsiteButton />
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => this.store.gotoLink(developer.linkedin)}
-            >
-              <LinkedinButton />
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.contentDescription}>{developer.description}</Text>
-        </View>
-        <View style={styles.TEAMTabs}>
-          <AnimatedTabs
-            height={55}
-            tabTitles={_.map(TEAM, dev => dev.name)}
-            onChangeTab={this.store.handleTabChange}
-          />
-        </View>
-      </View>
-    )
-  }
+	render() {
+		const navHeightPadding = { marginTop: Platform.OS === 'ios' ? 64 : 54 }
+		const developer = TEAM[this.state.currentTab]
+		return (
+			<View style={[styles.container, navHeightPadding]}>
+				<Image style={styles.profileImage} source={{ uri: developer.avatar }} />
+				<Image source={{ uri: developer.header }} style={styles.headerImage} />
+				<View style={styles.content}>
+					<Text style={styles.contentName}>{developer.name}</Text>
+					<Text style={styles.contentTitle}>{developer.title}</Text>
+					<View style={styles.socialButtons}>
+						<TouchableOpacity
+							activeOpacity={0.7}
+							onPress={() => this.store.gotoLink(developer.github)}
+						>
+							<GithubButton />
+						</TouchableOpacity>
+						<TouchableOpacity
+							activeOpacity={0.7}
+							onPress={() => this.store.gotoLink(developer.website)}
+						>
+							<WebsiteButton />
+						</TouchableOpacity>
+						<TouchableOpacity
+							activeOpacity={0.7}
+							onPress={() => this.store.gotoLink(developer.linkedin)}
+						>
+							<LinkedinButton />
+						</TouchableOpacity>
+					</View>
+					<Text style={styles.contentDescription}>{developer.description}</Text>
+				</View>
+				<View style={styles.TEAMTabs}>
+					<AnimatedTabs
+						height={55}
+						tabTitles={_.map(TEAM, dev => dev.name)}
+						onChangeTab={this.store.handleTabChange}
+					/>
+				</View>
+			</View>
+		)
+	}
 }
